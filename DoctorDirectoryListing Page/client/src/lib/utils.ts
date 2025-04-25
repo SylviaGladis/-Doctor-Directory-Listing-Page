d477913bb22bca330478a71d4d0032ffef05a8a7
Sylvia Gladis M <127619274+SylviaGladis@users.noreply.github.com>
2025-04-25 18:52:23 +0530

    Add files via upload

---
+++ DoctorDirectoryListing Page/client/src/lib/utils.ts
@@ -0,0 +1,51 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function debounce<F extends (...args: any[]) => any>(
+  func: F,
+  waitFor: number
+): (...args: Parameters<F>) => void {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: Parameters<F>): void => {
+    if (timeout !== null) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => func(...args), waitFor);
+  };
+}
+
+export const SPECIALTIES = [
+  "General Physician",
+  "Dentist",
+  "Dermatologist",
+  "Paediatrician",
+  "Gynaecologist",
+  "ENT",
+  "Diabetologist",
+  "Cardiologist",
+  "Physiotherapist",
+  "Endocrinologist",
+  "Orthopaedic",
+  "Ophthalmologist",
+  "Gastroenterologist",
+  "Pulmonologist",
+  "Psychiatrist",
+  "Urologist",
+  "Dietitian/Nutritionist",
+  "Psychologist",
+  "Sexologist",
+  "Nephrologist",
+  "Neurologist",
+  "Oncologist",
+  "Ayurveda",
+  "Homeopath",
+];
+
+export function getSpecialtyTestId(specialty: string): string {
+  return `filter-specialty-${specialty.replace(/\//g, "-")}`;
+}
